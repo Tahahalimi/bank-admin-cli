5a288caefd0c1ee7bfe30baa59ed84dc25ab770a
Tahahalimi <halimitaha85@gmail.com>
2026-02-16 16:40:46 +0330

    Release v1.0 - Bank CLI stable version

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+from colorama import Fore, Style
 
 conn = sqlite3.connect("data.db")
 cursor = conn.cursor()
@@ -23,7 +23,17 @@ class Account:
         self.idNumber = idNumber
         self.balance = balance
 
-
+def show_menu():
+    print(Fore.CYAN + '''
+1. Open new account
+2. Account information
+3. Withdraw
+4. Deposit
+5. Transfer
+6. Check balance
+7. Exit
+''' + Style.RESET_ALL)
+    
 def add_account(account):
     conn = sqlite3.connect("data.db")
     cursor = conn.cursor()
@@ -37,8 +47,8 @@ def add_account(account):
     acc_id = cursor.lastrowid
     account_number = f"9{acc_id:05}"
     conn.close()
-    print(f"Account for {account.name} added to database!")
-    print(f"The account number is {account_number}")
+    print(Fore.GREEN + f"Account for {account.name} added to database!")
+    print(Fore.GREEN + f"The account number is {account_number}")
 
 
 def show_accounts():
@@ -88,10 +98,10 @@ def transaction(sendId,receiveid, amount):
         cursor.execute("UPDATE accounts SET balance = ? WHERE id = ?",(rNew,receiveid))
         conn.commit()
         conn.close()
-        print(f"Transaction is complete! Sender new balance: {sNew}$")
+        print(Fore.GREEN + f"Transaction is complete! Sender new balance: {sNew}$")
     else:
-        print("Sender balance is not enough to do the transaction")
-        print(f"Sender Balnce: {senderBalnce}$")
+        print(Fore.RED + "Sender balance is not enough to do the transaction")
+        print(Fore.RED + f"Sender Balnce: {senderBalnce}$")
 
 def check_Balance(accid):
     conn = sqlite3.connect("data.db")
@@ -101,9 +111,9 @@ def check_Balance(accid):
 
     if result:
         balance = result[0]
-        print(f"balance: {balance}$")
+        print(Fore.GREEN + f"balance: {balance}$")
     else:
-        print("Account not found!")
+        print(Fore.RED + "Account not found!")
 
     conn.close()
 def updateBalance(accid,amount,operation):
@@ -119,29 +129,29 @@ def updateBalance(accid,amount,operation):
             newBalance = balance + amount
             cursor.execute("UPDATE accounts SET balance = ? WHERE id = ?",(newBalance,accid))
             conn.commit()
-            print(f"Deposit successful. New balance:{newBalance}$")
+            print(Fore.GREEN + f"Deposit successful. New balance:{newBalance}$")
         elif operation == "3":
             if balance >= amount:
                 newBalance = balance - amount
                 cursor.execute("UPDATE accounts SET balance = ? WHERE id = ?",(newBalance,accid))
                 conn.commit()
-                print(f"withdraw successful. New balance:{newBalance}$")
+                print(Fore.GREEN + f"withdraw successful. New balance:{newBalance}$")
             else:
-                print(f"Not enough balance! Current balance: {balance}$")
+                print(Fore.RED + f"Not enough balance! Current balance: {balance}$")
     else:
         print("Account not found!")
     conn.close()
 
+
+print(Fore.MAGENTA + r'''
+=========================
+      BANK CLI
+        V1.0
+=========================
+''' + Style.RESET_ALL)
+
 while True:
-    print('''
-1.open new account
-2.Accont information
-3.withdraw
-4.Deposit
-5.Transfer
-6.Check balance
-7.Exit
-        ''')
+    show_menu()
     toDo = input("what should I do for you? ")
     if toDo == "1":
         Name = input("Enter the First Name of the user: ")
@@ -154,28 +164,35 @@ while True:
         num = convert_id(num)
 
         if num is None:
-            print("Invalid Account number")
+            print(Fore.RED + "Invalid Account number")
+            print()
         else:
             acc = get_account(num)
 
             if acc:
-                print(f'''
-Name = {acc[1]}  {acc[2]} 
-National id Number = {acc[3]}
-Balance = {acc[4]}$
-    ''')
+                print(Fore.GREEN + f'''
+============================
+Account Information
+============================
+Name       : {acc[1]} {acc[2]}
+National ID: {acc[3]}
+Balance    : {acc[4]}$
+============================
+''')
             else:
-                print("Account not found")
+                print(Fore.RED + "Account not found")
     if toDo =="3":
         accid = input("Enter The account id: ")
         amount = input("Enter the amount you want withdraw: ")
         accid = convert_id(accid)
         updateBalance(accid,amount,"3")
+        print()
     if toDo =="4":
         accid = input("Enter The account id: ")
         amount = input("Enter the amount you want Deposit: ")
         accid = convert_id(accid)
         updateBalance(accid,amount,"4")
+        print()
     if toDo =="5":
         senderAcc = input("Enter the sender account: ")
         receiverAcc = input("Enter receiver account: ")
@@ -184,13 +201,15 @@ Balance = {acc[4]}$
         receiverAcc = convert_id(receiverAcc)
         if senderAcc and receiverAcc:
             transaction(senderAcc,receiverAcc,amount)
+            print()
         else:
-            print("something went wrong double check the accounts numbers")
+            print(Fore.RED + "something went wrong double check the accounts numbers")
+            print()
     if toDo =="6":
         accid = input("Enter the accont id: ")
         accid = convert_id(accid)
         check_Balance(accid)
-
+        print()
     if toDo == "7":
-        print("Goodbye")
+        print(Fore.YELLOW + "Goodbye")
         break
